d37a4438a526549333bdb32a7a3c1a31c4e2bb38
Parth0124 <abhangparth@gmail.com>
2024-12-17 03:55:48 +0530

    completing admin sidebar

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+const Admin = () => {
+
+    const router = useRouter()
+    useEffect(() => {
+        router.push("/admin/dashboard")
+    },[router])
+  return null
+}
+
+export default Admin
