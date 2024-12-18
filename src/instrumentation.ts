7bd1e46e0553b31275963fcbb6cc58278ddbeb3b
Parth0124 <abhangparth@gmail.com>
2024-12-18 07:27:04 +0530

    installed redis. initializing the queue using redis and bullmq

---
+++ src/instrumentation.ts
@@ -0,0 +1,18 @@
+import prisma from "./lib/prisma";
+
+export const register = async() => {
+  //This if statement is important, read here: https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+
+    const { Worker } = await import("bullmq");
+    const { connection, jobsQueue } = await import("@/lib");
+
+    new Worker("jobsQueue", async (job) => {
+        
+    },
+      {
+        connection, concurrency: 10, removeOnComplete: { count: 1000 }, removeOnFail: { count: 5000 },
+      }
+    );
+  }
+};
